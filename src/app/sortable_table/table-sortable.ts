1f361acbf4695fbd3f46c1107a39e8882a607915
Turunen Tomi <tomi.turunen@tieto.com>
2019-09-24 16:54:33 +0300

    sorttaus toimii

--- src/app/sortable_table/table-sortable.ts
+++ src/app/sortable_table/table-sortable.ts
@@ -1,4 +1,5 @@
 import { Component, Directive, EventEmitter, Input, Output, QueryList, ViewChildren } from '@angular/core';
+import { RecordsService } from '../records.service';
 
 interface Country {
   id: number;
@@ -8,39 +9,10 @@ interface Country {
   population: number;
 }
 
-const COUNTRIES: Country[] = [
-  {
-    id: 1,
-    name: 'Russia',
-    flag: 'f/f3/Flag_of_Russia.svg',
-    area: 17075200,
-    population: 146989754
-  },
-  {
-    id: 2,
-    name: 'Canada',
-    flag: 'c/cf/Flag_of_Canada.svg',
-    area: 9976140,
-    population: 36624199
-  },
-  {
-    id: 3,
-    name: 'United States',
-    flag: 'a/a4/Flag_of_the_United_States.svg',
-    area: 9629091,
-    population: 324459463
-  },
-  {
-    id: 4,
-    name: 'China',
-    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    area: 9596960,
-    population: 1409517397
-  }
-];
+const COUNTRIES: Country[] = [];
 
 export type SortDirection = 'asc' | 'desc' | '';
-const rotate: {[key: string]: SortDirection} = { 'asc': 'desc', 'desc': '', '': 'asc' };
+const rotate: { [key: string]: SortDirection } = { 'asc': 'desc', 'desc': '', '': 'asc' };
 export const compare = (v1, v2) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
 
 export interface SortEvent {
@@ -64,7 +36,7 @@ export class NgbdSortableHeader {
 
   rotate() {
     this.direction = rotate[this.direction];
-    this.sort.emit({column: this.sortable, direction: this.direction});
+    this.sort.emit({ column: this.sortable, direction: this.direction });
   }
 }
 
@@ -73,12 +45,37 @@ export class NgbdSortableHeader {
   templateUrl: './table-sortable.html'
 })
 export class NgbdTableSortable {
+  JOKERIT_ID: number = 109;
+  errorMessage: string;
+  imageWidth = 50;
+  flagWidth = 40;
+  imageMargin = 2;
+  scores: any[] = [];
+  scoreStorage: any[] = [];
+  //Todo try get id:s automatically
+  jokeritPlayerIds: any[]
+  countries = this.scores;
+  constructor(private recordsService: RecordsService) { }
 
-  countries = COUNTRIES;
+  async ngOnInit(): Promise<void> {
+    this.getOnePage();
+    console.log(4);
+  }
+  getOnePage(): void {
+    this.recordsService.getScores().subscribe({
+      next: player => {
+        console.log(player);
+        this.scores = player;
+        this.scoreStorage = player;
+      },
+      error: err => this.errorMessage = err
+    })
+  }
 
   @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
 
-  onSort({column, direction}: SortEvent) {
+  onSort({ column, direction }: SortEvent) {
+    console.log(column, direction)
 
     // resetting other headers
     this.headers.forEach(header => {
@@ -89,12 +86,23 @@ export class NgbdTableSortable {
 
     // sorting countries
     if (direction === '') {
-      this.countries = COUNTRIES;
-    } else {
-      this.countries = [...COUNTRIES].sort((a, b) => {
-        const res = compare(a[column], b[column]);
+      this.scores = this.scoreStorage;
+    } else if (column.includes('stats')) {
+      console.log(column.split("_")[1])
+      let sortingString: string = column.split("-")[1];
+      this.scores = [...this.scoreStorage].sort((a, b) => {
+        const aValue = (a.player.stats.find(s => s.id === sortingString)).val;
+        const bValue = (b.player.stats.find(s => s.id === sortingString)).val;
+        const res = compare(aValue, bValue);
         return direction === 'asc' ? res : -res;
       });
+    }
+    else {
+      this.scores = [...this.scoreStorage].sort((a, b) => {
+        const res = compare(a.player[column], b.player[column]);
+        return direction === 'asc' ? res : -res;
+      });
+      console.log(this.scores)
     }
   }
 
