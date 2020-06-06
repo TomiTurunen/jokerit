bc6f3b07b5fcc165b08fd1768e440436a460dfd3
TomiTurunen <lich87@gmail.com>
2020-06-06 09:57:11 +0300

    Data secerity update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Navbar } from './navbar/navbar'
-import { RecordsService } from './records.service';
+import { Component, OnInit } from "@angular/core";
+//import { Navbar } from './navbar/navbar'
+import { RecordsService } from "./records.service";
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['./app.component.sass'],
-  templateUrl: './navbar/navbar.html',
+  selector: "app-root",
+  styleUrls: ["./app.component.sass"],
+  templateUrl: "./navbar/navbar.html"
 })
 export class AppComponent implements OnInit {
-  public qwerty: string = '242';
+  public qwerty: string = "242";
   containerActive = false;
   //TODO get activates work
   ngOnInit(): void {
-    console.log("123", window.location.href)
+    console.log("123", window.location.href);
   }
   clicked(event) {
     this.containerActive = !this.containerActive;
@@ -25,4 +25,3 @@ export class AppComponent implements OnInit {
     event.target.condition = true;
   }
 }
-
